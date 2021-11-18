af6f97c5792b0152ac24379dc0b85160f9ec6013
murcoder <chris.murauer@yahoo.de>
2021-11-18 15:24:09 +0100

    storno

--- pages/me/workshopBookings.vue
+++ pages/me/workshopBookings.vue
@@ -15,14 +15,9 @@
           :key="workshopDate.content.workshop.uuid"
           class="preview"
         />
-        <!--
-        <button
-            class="input-button-primary"
-            @click="storno(workshopDate)"
-        >
-          Diesen Workshop stornieren
-        </button>
-        -->
+<!--        <button class="input-button-primary" @click="storno(workshopDate)">-->
+<!--          Stornieren-->
+<!--        </button>-->
         <workshop-dates
           :dates="[workshopDate]"
           class="workshop-dates"
@@ -56,23 +51,26 @@ export default {
   },
   methods: {
     // This function is for the commented out storno-button. That gives user the possibility to cancel their workshops
-    storno: function (workshopDate) {
+    storno (workshopDate) {
       const data = {
         workshop_date_id: workshopDate.uuid
       }
       if (confirm('Workshop ' + workshopDate.content.workshop.name + ' wirklich stornieren?')) {
-        this.$store.dispatch('workshopStorno', data).then((data) => {
-          if (data.status >= 200 && data.status < 300) {
-            this.$toast.show('Der Workshop wurde erfolgreich storniert!', {
-              className: 'goodToast'
+        this.$store.dispatch('workshopStorno', data)
+          .then((data) => {
+            console.log('status', status)
+            if (data.status >= 200 && data.status < 300) {
+              this.$toast.show('Der Workshop wurde erfolgreich storniert!', {
+                className: 'goodToast'
+              })
+            }
+          }).catch((error) => {
+            console.log(error.response.status, error.response.data.error)
+            this.$sentry.captureException(new Error(error))
+            this.$toast.show(error.response.data.msg, {
+              className: 'badToast'
             })
-          }
-        }, (err) => {
-          this.$sentry.captureException(new Error(err))
-          this.$toast.show('Fehler! Workshops koennen maximal 72h vor Beginn storniert werden. Sollte das nicht der Fall sein, wende dich an den Frontdesk.', {
-            className: 'badToast'
           })
-        })
       }
     }
   }
@@ -81,12 +79,14 @@ export default {
 
 <style lang="scss" scoped>
 @import '/assets/scss/styles.scss';
-.workshops{
-  display:flex;
+
+.workshops {
+  display: flex;
   flex-direction: column;
   align-items: center;
 }
-.workshop-item{
+
+.workshop-item {
   max-width: 50%;
 }
 </style>
