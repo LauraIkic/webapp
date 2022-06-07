0498a8f056eb4c37d8300728ce5c63530223808e
murcoder <chris.murauer@yahoo.de>
2022-06-07 13:04:55 +0200

    added dom watcher

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -2,7 +2,7 @@
  * @see https://fontawesome.com/docs/web/use-with/vue/use-with
  */
 import Vue from 'vue'
-import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { library, config, dom } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,6 +12,7 @@ config.autoAddCss = false
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
 library.add(fas)
+dom.watch()
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
