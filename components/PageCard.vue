79495e52add132e61a2b672a4e7052d3de209db3
Ines2108 <ines.niedermaier@gmail.com>
2022-08-19 16:20:27 +0200

    Create PageList & PageCard

---
+++ components/PageCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+      v-editable="blok"
+      class="page-card"
+  >
+    <nuxt-link :to="'/' + blok.link.cached_url">
+      <section class="display-item">
+        <div class="top">
+          <div class="top-image" :style="{ 'background-image': 'url(' + $resizeImage(blok.image, '1500x1500') + ')' }"/>
+        </div>
+        <div class="bottom">
+          <div class="bottom-text">
+            {{blok.text}}
+          </div>
+        </div>
+      </section>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageCard',
+  props: ['blok']
+}
+</script>
+
+<style lang="scss" scoped>
+.display-item {
+  border-radius: 15px;
+  height: 230px;
+  display: flex;
+  background: black;
+  width: 370px;
+  box-shadow: 10px 5px 5px #00000024;
+  flex-flow: column;
+  @include media-breakpoint-down(md) {
+    height: 200px;
+    width: 300px;
+    margin: 5% 0;
+  }
+
+  .top {
+    height: inherit;
+    color: white;
+
+    .top-image {
+      background-size: cover;
+      background-position: 50%;
+      background-repeat: no-repeat;
+      height: 175px;
+      border-top-left-radius: 15px;
+      border-top-right-radius: 15px;
+      @include media-breakpoint-down(md) {
+        height: 155px;
+      }
+    }
+  }
+
+  .bottom {
+    height: 45%;
+    background: white;
+    align-self: end;
+    width: inherit;
+    border-bottom-right-radius: 13px;
+    border-bottom-left-radius: 13px;
+
+    .bottom-text {
+      text-align: center;
+      margin: 3% 0;
+    }
+  }
+}
+</style>
---
+++ components/PageList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-editable="blok"
+    class="page-list"
+  >
+    <page-card
+      v-for="i in blok.items"
+      :key="i.uid"
+      :blok="i"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['blok'],
+  created () {
+  }
+}
+</script>
+
+<style lang="scss">
+
+.page-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 8%;
+  @media screen and (max-width: 799px) {
+    flex-direction: column;
+    align-items: center;
+    margin: 5%;
+  }
+}
+</style>
