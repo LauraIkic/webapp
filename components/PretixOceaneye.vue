1ddab3b507dadfcdac125a458f2e4114dd71c4d1
Ines2108 <ines.niedermaier@gmail.com>
2022-08-30 15:37:49 +0200

    Create new pretix oceaneye component

--- components/PretixOceaneye.vue
+++ components/PretixOceaneye.vue
@@ -1,27 +1,26 @@
 <template>
-  <div v-editable="blok"
-       class="pretix-oceaneye"
-  >
-    <link rel="stylesheet" type="text/css" href="https://pretix.eu/grandgarage/oceanEye/widget/v1.css">
-    <script type="text/javascript" src="https://pretix.eu/widget/v1.de-informal.js" async></script>
-    <div class="content">
-      <div class="header">
-        <h2 class="title">
-          {{ blok.Headline }}
-        </h2>
-      </div>
-      <div class="body">
-        <pretix-widget name="pretix" event="https://pretix.eu/grandgarage/oceanEye/"></pretix-widget>
+  <section class="workshop-overview">
+    <div
+        v-editable="blok"
+        class="pretix-oceaneye"
+    >
+      <link rel="stylesheet" type="text/css" href="https://pretix.eu/grandgarage/oceanEye/widget/v1.css">
+      <script type="text/javascript" src="https://pretix.eu/widget/v1.de.js" async></script>
+      <div class="pretix-content">
+        <div>
+          <pretix-widget name="pretix" event="https://pretix.eu/grandgarage/oceanEye/"></pretix-widget>
+        </div>
         <noscript>
           <div class="pretix-widget">
             <div class="pretix-widget-info-message">
-              JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" href="https://pretix.eu/grandgarage/oceanEye/">hier</a>.
+              JavaScript is disabled in your browser. To access our ticket shop without JavaScript,
+              please <a target="_blank" href="https://pretix.eu/ggTest">click here</a>.
             </div>
           </div>
         </noscript>
       </div>
-    </div>
-  </div>
+      </div>
+  </section>
 </template>
 
 <script>
@@ -30,30 +29,37 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.pretix-widget-wrapper{
-  width: 1264px;
-  background: white;
-}
-.content {
-  padding: 1.5em;
+<style lang="scss">
+.pretix-content {
+  display: flex;
+  justify-content: center;
   margin: 5% 0;
-  .header {
-    display: flex;
-    align-items: flex-start;
-
-    .title {
-      flex: 5;
-      font-weight: normal;
-      margin: 0;
-      font-family: $font-secondary;
-      min-height: 2em;
+  .pretix-widget-wrapper{
+    a {
+      color: $color-secondary;
+      text-decoration: none;
     }
-  }
-  .body {
-    font-size: 1rem;
-    letter-spacing: 0.03em;
-    line-height: 1.4;
+    a:hover {
+      color: $color-secondary;
+      text-decoration: underline;
+    }
+    button {
+      background-color: $color-secondary;
+      border-color: $color-secondary;
+    }
+    button:hover {
+        background-color: black;
+        border-color: black;
+    }
+    width: 70vw;
+    max-width: 1264px;
+    background: white;
+    .pretix-widget-event-calendar-previous-month{
+      padding: 10px;
+    }
+    .pretix-widget-event-calendar-next-month{
+      padding: 10px;
+    }
   }
 }
 </style>
