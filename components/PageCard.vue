93411b234d7cdc0dcd5e6ec9b3b5b15427084b61
Ines2108 <ines.niedermaier@gmail.com>
2022-09-08 11:33:57 +0200

    Changes in page card component

--- components/PageCard.vue
+++ components/PageCard.vue
@@ -6,7 +6,6 @@
     <nuxt-link :to="'/' + blok.link.cached_url">
       <section
           class="display-item"
-          :class="{length : length}"
       >
         <div class="top">
           <div class="top-image" :style="{ 'background-image': 'url(' + $resizeImage(blok.image, '1500x1500') + ')' }"/>
@@ -24,34 +23,13 @@
 <script>
 export default {
   name: 'PageCard',
-  props: ['blok'],
-  computed: {
-    length () {
-      return this.blok.length > 1
-    }
-  }
+  props: ['blok']
+
 }
 </script>
 
 <style lang="scss" scoped>
 .display-item {
-  .length {
-    width: 250px;
-  }
-  border-radius: 15px;
-  height: 230px;
-  display: flex;
-  background: white;
-  width: 370px;
-  box-shadow: 10px 5px 5px #00000024;
-  flex-flow: column;
-  overflow: hidden;
-  @include media-breakpoint-down(md) {
-    height: 200px;
-    width: 300px;
-    margin: 5% 0;
-  }
-
   .top {
     .top-image {
       background-size: cover;
@@ -60,7 +38,6 @@ export default {
       border-top-left-radius: 15px;
       border-top-right-radius: 15px;
       height: 175px;
-      width: 370px;
       transition: transform 0.4s ease;
       transform-origin: 50% 50%;
       @include media-breakpoint-down(md) {
@@ -77,7 +54,7 @@ export default {
 
     .bottom-text {
       text-align: center;
-      margin: 3% 0;
+      margin: 4% 0;
       color: black;
     }
   }
--- components/PageList.vue
+++ components/PageList.vue
@@ -8,6 +8,7 @@
       :key="i.uid"
       :blok="i"
       class="page-card"
+      :class="{ small : length }"
     />
   </div>
 </template>
@@ -16,6 +17,11 @@
 export default {
   props: ['blok'],
   created () {
+  },
+  computed: {
+    length () {
+      return this.blok.items.length > 2
+    }
   }
 }
 </script>
@@ -26,14 +32,32 @@ export default {
   display: flex;
   justify-content: center;
   padding: 30px;
-  margin: 5% 0 2% 0;
+  margin: 6% 0 1% 0;
   @media screen and (max-width: 799px) {
     flex-direction: column;
     align-items: center;
-    margin: 5%;
+    margin: 1% 0%;
+  }
+  .page-card.small {
+    width: 330px;
   }
   .page-card {
     margin: 0 2%;
+    border-radius: 15px;
+    height: 230px;
+    display: flex;
+    background: white;
+    width: 370px;
+    box-shadow: 10px 5px 5px #00000024;
+    flex-flow: column;
+    overflow: hidden;
+    @include media-breakpoint-down(md) {
+      height: 200px;
+      width: 300px;
+    }
+    @include media-breakpoint-down(sm) {
+      margin: 3% 0;
+    }
   }
 }
 </style>
