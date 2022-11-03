526fbf294ecb868ab2d7aeeeaa2d804b63fa574a
0856344 <Sebastian.Kaltenboeck@gmx.at>
2022-11-03 16:11:00 +0100

    remove pretix widget (debugging)

--- components/PretixCalendar.vue
+++ components/PretixCalendar.vue
@@ -32,7 +32,7 @@
         <noscript>
           <div class="pretix-widget">
             <div class="pretix-widget-info-message">
-              JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte <a target="_blank" rel="noopener" :href="event">hier</a>.
+              <p> JavaScript ist in Ihrem Browser deaktiviert. Um unseren Ticket-Shop ohne JavaScript aufzurufen, klicken Sie bitte</p> <a target="_blank" rel="noopener" :href="event">hier</a>.
             </div>
           </div>
         </noscript>
