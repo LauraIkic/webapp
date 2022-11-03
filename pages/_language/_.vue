95e02bd2d954510212924253f25df7aa83ad8630
murcoder <chris.murauer@yahoo.de>
2022-11-03 16:09:32 +0100

    added snowflake icon for makemas workshops

--- pages/_language/_.vue
+++ pages/_language/_.vue
@@ -11,8 +11,13 @@
 
 <script>
 import storyblokLivePreview from '@/mixins/storyblokLivePreview'
+import Snowflakes from 'magic-snowflakes'
+const snowflakes = new Snowflakes({ color: '#fdfdfd', count: 500 })
 
 export default {
+  created () {
+    snowflakes.start()
+  },
   mixins: [storyblokLivePreview],
   asyncData (context) {
     const path = '/' + context.route.params.pathMatch
