f7006ed8e98a7693f3a6487d02ea4a1396764221
0856344 <Sebastian.Kaltenboeck@gmx.at>
2022-11-03 09:57:56 +0100

    compatiblity fix with image-slideshow changes (resizeImage bug) https://api.storyblok.com/v2/cdn/stories/de/workshops/lasercut-msu-juli-2021?cv=1667462307&token=dKj29ERxJ5kHRQul7QKVtwtt&version=published https://api.storyblok.com/v2/cdn/stories/de/workshops/3d-druck-xmas-nov-2022?cv=1667462307&token=dKj29ERxJ5kHRQul7QKVtwtt&version=published

--- plugins/helper.js
+++ plugins/helper.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.prototype.$resizeImage = function (str, param) {
+  // ensure compatiblity with image-slideshow changes in storyblok (schema changes)
+  if (str?.filename) {
+    return typeof str.filename === 'undefined' ? '' : str.filename.replace(/a.storyblok.com/g, 'img2.storyblok.com/' + param)
+  }
   return typeof str === 'undefined' ? '' : str.replace(/a.storyblok.com/g, 'img2.storyblok.com/' + param)
 }
 
