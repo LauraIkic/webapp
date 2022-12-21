6464ce4af24135c8e491d68eddea8007b91a9cf1
murcoder <chris.murauer@yahoo.de>
2022-12-21 11:30:41 +0100

    removed makemas

--- pages/_language/workshops/index.vue
+++ pages/_language/workshops/index.vue
@@ -68,7 +68,7 @@ export default {
         { key: 'workshop', name: 'Workshop', value: false },
         { key: 'training', name: 'Einschulungen', value: false },
         { key: 'frauenundtechnik', name: '#frauenundtechnik', value: false },
-        { key: 'makemas', name: '#makemas2022', value: false }
+        // { key: 'makemas', name: '#makemas2022', value: false }
       ],
       loading: false,
       search: '',
