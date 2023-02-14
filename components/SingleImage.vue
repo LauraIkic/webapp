e89c95766f30fa0fb1822cdfd6f9e0614efe7b8b
Laura Ikic <laura.ikic@grandgarage.eu>
2023-02-14 15:12:26 +0100

    Remove css

--- components/SingleImage.vue
+++ components/SingleImage.vue
@@ -22,41 +22,6 @@ export default {
 
 <style lang="scss" scoped>
 
-.headline {
-  font-size: 1.6em;
-  @include media-breakpoint-up(lg) {
-    width: 700px;
-    font-size: 2.7em;
-  }
-  position: relative;
-  margin-left: 7%;
-  letter-spacing: 2px;
-  line-height: 1.5;
-  font-weight: 600;
-  font-family: $font-secondary;
-
-  p {
-    margin: 0;
-  }
-}
-
-.text {
-  @include media-breakpoint-up(lg) {
-    width: 900px;
-    margin: 0 0 2% 12%;
-  }
-  @include media-breakpoint-down(md) {
-    margin: 3% 15% 10%;
-    font-size: 0.9em;
-    line-height: 1.6;
-  }
-  margin-top: 0;
-  font-weight: normal;
-  font-family: $font-primary;
-  line-height: 1.6;
-  font-size: 1.1em;
-  letter-spacing: .03em;
-}
 .row {
   display: flex;
   flex-direction: row; //default
@@ -87,8 +52,5 @@ export default {
   max-width: 100%;
   max-height: 100%;
 }
-.column {
-  padding: 0.3em;
-  flex: 1;
-}
+
 </style>
